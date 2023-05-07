493e1f6efafed4671aef79175be51f4ce0afd337
master <eoals6511@naver.com>
2023-05-08 01:14:31 +0900

    20230508

---
+++ vue/20230504/과제 5/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <p>Counter: {{ counter }}</p>
+    <p>Counter x2: {{ counterDouble }}</p>
+    <p></p>
+    <button @click="increase">increase +</button>
+    <button @click="decrease">decrease -</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App', 
+  data: function() {
+    return {
+      
+    }
+  },
+  methods: {
+    increase: function () {
+      this.$store.commit('increase' )
+    },
+    decrease: function () {
+      this.$store.commit('decrease' )
+    },
+  },
+  computed: {
+    counter () {
+      return this.$store.state.counter
+    },
+    counterDouble: function () {
+      return this.$store.getters.counterDouble
+    },
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
