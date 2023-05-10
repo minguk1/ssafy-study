72d710d3724984f26579f74fed4c9b492ebf4c48
duljji <eoals6511@naver.com>
2023-05-10 17:59:45 +0900

    2323

--- algo/백준 16236 아기상어.py
+++ algo/백준 16236 아기상어.py
@@ -12,18 +12,18 @@ stack = 0
 time = 0
 total_time = 0
 
-di = [-1, 1, 0, 0]
-dj = [0, 0, -1, 1]
-def upgrade_able(size, stack):
-    cnt = 0
-    for i in range(n):
-        for j in range(n):
-            if 1 <= sea[i][j] < size:
-                cnt += 1
-    if cnt+stack < size:
-        return False
-    else:
-        return True
+di = [-1, 0, 0, 1]
+dj = [0, -1, 1, 0]
+# def upgrade_able(size, stack):
+#     cnt = 0
+#     for i in range(n):
+#         for j in range(n):
+#             if 1 <= sea[i][j] < size:
+#                 cnt += 1
+#     if cnt+stack < size:
+#         return False
+#     else:
+#         return True
 def eat_able(size):
     for i in range(n):
         for j in range(n):
@@ -32,8 +32,9 @@ def eat_able(size):
     return False
 
 def eat_fish(i,j,size, stack, time):
-    print("시작", time, i, j)
+
     global total_time
+    print("시작",  i, j, total_time)
 
     visited = [[0]*n for _ in range(n)]
     visited[i][j] = 1
@@ -50,12 +51,13 @@ def eat_fish(i,j,size, stack, time):
                 print("먹음", nr, nc)
                 stack += 1
                 time = visited[r][c]
-                total_time += time
+                total_time += visited[r][c]
                 sea[nr][nc] = 0
                 if stack == size:
-                    print("진화")
+
                     stack = 0
                     size += 1
+                    print("진화", size)
                     if eat_able(size) == True:
 
                         eat_fish(nr, nc, size, stack, time)
@@ -63,23 +65,23 @@ def eat_fish(i,j,size, stack, time):
                     else:
                         print(time)
                         # print("end")
-                        return time
+                        return
                 else:
                     if eat_able(size) == True:
-                        print("더먹기가능")
-                        print(time, "time")
+                        # print("더먹기가능")
+                        # print(time, "time")
 
                         eat_fish(nr, nc, size, stack, time)
                         return
                     else:
 
-                        print(time)
-                        return time
+                        # print(time)
+                        return
             elif (0<=nr<n and 0<=nc<n) and visited[nr][nc]==0 and (sea[nr][nc] == 0 or sea[nr][nc] == size):
                 time += 1
                 visited[nr][nc] = visited[r][c] + 1
                 q.append((nr, nc))
-                # print("append")
+                print("append", nr, nc)
 
 eat_fish(r,c,2,0,0)
 print(total_time, "최종시간")
