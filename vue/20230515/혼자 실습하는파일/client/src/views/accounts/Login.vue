35881a5071235172677442942cebb59a612a2681
duljji <eoals6511@naver.com>
2023-05-15 18:00:01 +0900

    20230515

---
+++ vue/20230515/혼자 실습하는파일/client/src/views/accounts/Login.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h3>username : <input type="text" v-model="username"></h3>
+    <h3>password : <input type="text" v-model="password"></h3>
+    
+    <button @click="login"> 입력 완료 </button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Login',
+  data: function () {
+    return {
+      username : null,
+      password : null,
+    }
+  },
+  methods: {
+    login: function () {
+      axios({
+        method : 'post',
+        url : 'http://127.0.0.1:8000/accounts/',
+        data : {username : this.username, password : this.password}
+      })
+      .then((res) => {
+        console.log(res)
+        // this.userinformations.push(res.data)
+        // this.$router.push({name : 'Login'})
+        
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    }
+  }
+}
+</script>
---
+++ vue/20230515/혼자 실습하는파일/client/src/views/todos/CreateTodo.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <input 
+      type="text" 
+      v-model.trim="title" 
+      @keyup.enter="createTodo"
+    >
+    <button @click="createTodo">+</button>
+  </div>
+</template>
+
+<script>
+import axios from'axios'
+const API_URL = " http://127.0.0.1:8000"
+
+export default {
+  name: 'CreateTodo',
+  data: function () {
+    return {
+      title: null,
+    }
+  },
+  methods: {
+    createTodo: function () {
+      axios({
+        method : 'post',
+        url : `${API_URL}/todos/`,
+        data : {title : this.title, iscompleted : false}
+
+      })
+      .then((res)=>{
+        console.log(res)
+        this.$router.push({name : 'TodoList'})
+
+      })
+      
+    }
+  }
+}
+</script>
